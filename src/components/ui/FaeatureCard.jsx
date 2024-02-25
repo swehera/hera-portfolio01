6b0381d40a2c2c5156f4bcf4225921fc2d53a47c
Your Name <you@example.com>
2024-02-25 23:59:24 +0600

    ok

--- src/components/ui/FaeatureCard.jsx
+++ src/components/ui/FaeatureCard.jsx
@@ -9,7 +9,7 @@ const FaeatureCard = ({ item }) => {
     >
       <span className="iconStyle">{item.icon}</span>
       <p className="text-xl font-bold">{item.title}</p>
-      <p className="text-base font-medium">{item.description}</p>
+      <p className="text-base font-medium pb-1">{item.description}</p>
 
       <FaArrowRight className=" text-2xl group-hover:absolute group-hover:bottom-8  text-primaryColor  group-hover:text-designColor translate-y-2  -translate-x-2 group-hover:translate-y-0 group-hover:translate-x-0 duration-300" />
     </div>
