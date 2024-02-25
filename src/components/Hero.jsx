ffc14461d8f33b5ff9f0c70a7ced6d48267ceb5f
Your Name <you@example.com>
2024-02-25 14:39:32 +0600

    change name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,8 @@ const Hero = () => {
               Full Stack developer
             </p>
             <h2 className="text-5xl md:text-7xl font-bold text-white">
-              Hi, I’m <span className="text-designColor">Lutfor Rahman</span>
+              Hi, I’m{" "}
+              <span className="text-designColor">Lutfor Rahman Hira</span>
             </h2>
             <h2 className="text-2xl md:text-4xl font-bold text-white">
               a{" "}
