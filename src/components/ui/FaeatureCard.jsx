46ba1d89fbfb6d81efa281b673c91fa51c1f37e3
Your Name <you@example.com>
2024-02-25 23:37:14 +0600

    responsive style

--- src/components/ui/FaeatureCard.jsx
+++ src/components/ui/FaeatureCard.jsx
@@ -10,9 +10,8 @@ const FaeatureCard = ({ item }) => {
       <span className="iconStyle">{item.icon}</span>
       <p className="text-xl font-bold">{item.title}</p>
       <p className="text-base font-medium">{item.description}</p>
-      
-        <FaArrowRight className=" text-2xl group-hover:absolute group-hover:bottom-8  text-primaryColor  group-hover:text-designColor  translate-y-2 -translate-x-2 group-hover:translate-y-0 group-hover:translate-x-0 duration-300" />
-      
+
+      <FaArrowRight className=" text-2xl group-hover:absolute group-hover:bottom-8  text-primaryColor  group-hover:text-designColor translate-y-1  md:translate-y-2 translate-x-1 md:-translate-x-2 group-hover:translate-y-0 group-hover:translate-x-0 duration-300" />
     </div>
   );
 };
