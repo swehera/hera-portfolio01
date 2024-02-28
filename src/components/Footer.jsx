79a8df11a07598aab500983dc6968ff0b527a45a
Your Name <you@example.com>
2024-02-28 07:01:03 +0600

    ok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Container from "./Container";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo1.png";
 import { FaFacebookSquare, FaLinkedin, FaGithubSquare } from "react-icons/fa";
 import SocialIcon from "./ui/SocialIcon";
 
