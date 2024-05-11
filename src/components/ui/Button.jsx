f1d989e3151503a03edf4a7337f91a2409d4771a
Your Name <you@example.com>
2024-05-11 21:07:57 +0600

    add-resume

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -4,14 +4,14 @@ import { resume } from "../../assets/getImage";
 const Button = (props) => {
   console.log(props.className);
   return (
-    <button
-      className={`text-designColor/70 text-sm  font-semibold bg-gradient-to-tr from-black to-primaryColor rounded-md hover:bg-gradient-to-t hover:from-primaryColor hover:to-black hover:text-designColor duration-300 hover:-translate-y-1 ${props.className}`}
-    >
-      <a href={resume} target="_blank">
+    <a href={resume} target="_blank">
+      <button
+        className={`text-designColor/70 text-sm  font-semibold bg-gradient-to-tr from-black to-primaryColor rounded-md hover:bg-gradient-to-t hover:from-primaryColor hover:to-black hover:text-designColor duration-300 hover:-translate-y-1 ${props.className}`}
+      >
         {" "}
         {props.title ? props.title : "Hire Me"}
-      </a>
-    </button>
+      </button>
+    </a>
   );
 };
 
